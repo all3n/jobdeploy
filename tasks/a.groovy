d4df1d63c56f279bf020dd07272531897739fe78
all3nwhc <wanghch8398@163.com>
2022-11-30 15:06:46 +0800

    remove useless file

--- tasks/a.groovy
+++
@@ -1,8 +0,0 @@
-import com.devhc.jobdeploy.DeployPlugin;
-import com.devhc.jobdeploy.App;
-public class Foo implements DeployPlugin{
-        @Override
-        public void run(App app){
-                println app.getDeployJson().getName();
-        }
-}
